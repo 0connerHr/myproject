ec53a4772a914960b2a7724a827d5cb3cd62b878
伊藤舞浩 <>
2025-07-05 23:04:28 +0900

    ログイン画面

--- demo/src/main/java/com/example/demo/controller/IndexController.java
+++ demo/src/main/java/com/example/demo/controller/IndexController.java
@@ -1,13 +1,21 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.demo.repository.UserRepository;
+
 @Controller
 public class IndexController {
+	@Autowired
+	UserRepository userRepository;
+	
 	@RequestMapping(path="/",method=RequestMethod.GET)
-		public String index() {
+		public String index(Model model) {
+			model.addAttribute("userInfo",userRepository.findAll());
 			return "login/login";
 		}
 	}
--- demo/src/main/java/com/example/demo/controller/InputController.java
+++ demo/src/main/java/com/example/demo/controller/InputController.java
@@ -1,13 +1,37 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+
 @Controller
 public class InputController {
-	@RequestMapping(path="/input",method=RequestMethod.POST)
-	public String Input(@Param String userName) {
-		return "input/input";
+	@Autowired
+	UserRepository userRepository;
+
+	@RequestMapping(path = "/input", method = RequestMethod.POST)
+	public String Input(String userName, String passward) {
+		List<User> userInfo = new ArrayList<>();
+		userInfo = userRepository.findAll();
+		for (User user : userInfo) {
+			System.out.println(user.getUser_name());
+			System.out.println(userName);
+			System.out.println(user.getPassward());
+			System.out.println(passward);
+			if (user.getUser_name().equals(userName) && user.getPassward().equals(passward)) {
+				System.out.println(1);
+				return "input/input";
+			}
+		}
+		return "redirect:/";
+
 	}
+
 }
